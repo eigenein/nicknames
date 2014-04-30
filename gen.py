5a8e8164997d8ae10f06827a9237736b77aaefcb
Pavel Perestoronin <eigenein@gmail.com>
2014-04-30 19:09:46 +0300

    Add gen.py

--- gen.py
+++ gen.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import random
+from __future__ import print_function
+
+import argparse
+
+
+def main(args):
+    for set_file in args.set_files:
+        generate_model(set_file)
+
+
+def generate_model(set_file):
+    print(set_file.name)
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(dest="set_files", nargs=argparse.REMAINDER, type=argparse.FileType("rt"))
+    args = parser.parse_args()
+    main(args)
