5e4fedbe9ba0096a52402560bd497788233a6125
Pavel Perestoronin <eigenein@gmail.com>
2014-05-02 00:19:29 +0300

    Trigrams

--- gen.py
+++ gen.py
@@ -4,6 +4,8 @@
 from __future__ import print_function
 
 import argparse
+import json
+import os
 
 
 def main(args):
@@ -12,8 +14,27 @@ def main(args):
 
 
 def generate_model(set_file):
-    print(set_file.name)
-    pass
+    "Generates model for the specified set file."
+
+    print("Set: %s" % set_file.name)
+    basename = os.path.basename(set_file.name)
+    coffee_name = os.path.join("coffee", "%s%scoffee" % (os.path.splitext(basename)[0], os.extsep))
+    
+    print("Generating %s…" % coffee_name)
+    model = {}
+    for word in set_file:
+        word = word.strip()
+
+
+def trigrams(word):
+    "Generates trigrams for the specified word."
+
+    current = "$" * 3
+    for char in word:
+        current = current[1:] + char
+        yield current
+    yield current[1:] + "$"
+    yield current[2:] + "$$"
 
 
 if __name__ == "__main__":
